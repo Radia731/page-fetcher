fd4636617a1d0460dc32df2027efe7f44fd3e2e5
Radia Ahmed <rad@Radias-MacBook-Air.local>
2023-04-28 09:30:01 -0400

    revision

---
+++ fetcher.js
@@ -0,0 +1,27 @@
+const request = require('request');
+const fs = require('fs');
+const command = process.argv.slice(2); // Input from node
+
+request(`${command[0]}`, (error, response, body) => {
+  const size = body.length; // Get the file size 1 byte per character
+
+  if (error) {
+    // Print the error if one occurred
+    console.log('error:', error);
+  }
+  if (response) {
+    // Print the response status code if a response was received
+    console.log('statusCode:', response && response.statusCode);
+  }
+
+  fs.writeFile(`${command[1]}`, body, err => {
+    // Write to file
+
+    if (err) {
+      // If error, output error message.
+      console.error(err);
+    }
+
+    console.log(`Downloaded and saved ${size} bytes to ${command[1]}`); // If success
+  });
+});
